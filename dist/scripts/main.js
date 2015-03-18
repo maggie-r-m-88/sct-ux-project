98280431f194f5db611ec8ac9cfd5b6e4a6fb45f
Maggie Martin <maggierozay@gmail.com>
2015-03-18 00:19:06 -0400

    going to bed

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -4,9 +4,14 @@ $("#menu-toggle").click(function(e) {
         $(this).find('i').toggleClass('fa-angle-left fa-angle-right');
 
 });
-$(".sidebar-nav-links li").click(function(e) {
+$(".sidebar-nav-links a").click(function(e) {
         e.preventDefault();
-        $(this).toggleClass("sidebar-active-tab");
+
+        $(this).children().addClass('sidebar-active-tab')
+         .parent().siblings().find('.sidebar-active-tab').removeClass('sidebar-active-tab');
+
+    //    $(this).addClass("sidebar-active-tab");
+      //  $(this).siblings().removeClass('sidebar-active-tab');
       //  $(this).find('').toggleClass('fa-angle-left fa-angle-right');
 
 });
