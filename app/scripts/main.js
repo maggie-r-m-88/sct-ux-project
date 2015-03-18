00b53e87ffb4aeab7635b5d2c5f5981515cf11ed
Maggie Martin <maggierozay@gmail.com>
2015-03-17 22:47:12 -0400

    finishing console

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,12 @@
 $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("active");
-        $(this).find('i').toggleClass('fa-caret-left fa-caret-right');
-      
+        $(this).find('i').toggleClass('fa-angle-left fa-angle-right');
 
-
+});
+$(".sidebar-nav-links li").click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass("sidebar-active-tab");
+      //  $(this).find('').toggleClass('fa-angle-left fa-angle-right');
 
 });
