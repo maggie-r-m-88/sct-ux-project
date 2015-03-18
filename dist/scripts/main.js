f6d035683927baf40779b5c3a027e8bd56eceba1
Maggie Martin <maggierozay@gmail.com>
2015-03-18 12:56:40 -0400

    tabs added

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -5,13 +5,9 @@ $("#menu-toggle").click(function(e) {
 
 });
 $(".sidebar-nav-links a").click(function(e) {
-      
+
 
         $(this).children().addClass('sidebar-active-tab')
          .parent().siblings().find('.sidebar-active-tab').removeClass('sidebar-active-tab');
-
-    //    $(this).addClass("sidebar-active-tab");
-      //  $(this).siblings().removeClass('sidebar-active-tab');
-      //  $(this).find('').toggleClass('fa-angle-left fa-angle-right');
 
 });
